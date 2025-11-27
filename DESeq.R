5beee0b62e1a5cf51dce5972e925f4efb69d4e5d
AGASH0 <agash.uthayakumar0@gmail.com>
2025-11-27 23:48:33 +0100

    snakemake avec code R

--- DESeq.R
+++ DESeq.R
@@ -1,67 +1,73 @@
-data <- read.delim("~/all_counts_with_header.txt", row.names=1)
+#!/usr/bin/env Rscript
 
-head(data)
-summary(data)
+# ------------------------------------------------------------------------------
+# Snakemake interface : récupère les fichiers du Snakefile
+# ------------------------------------------------------------------------------
+counts_file  <- snakemake@input[["counts"]]
+article_file <- snakemake@input[["article"]]
+output_dir   <- snakemake@params[["outdir"]]
 
-boxplot(data)
+dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
 
-# Suppose que ton dataframe s'appelle df
+# ------------------------------------------------------------------------------
+# Chargement des données
+# ------------------------------------------------------------------------------
+data <- read.delim(counts_file, row.names = 1)
+data_article <- read.delim(article_file)
+
 names(data)[1:3] <- paste0("per_", 1:3)
 names(data)[4:6] <- paste0("control_", 1:3)
 
-
-
-library(edgeR) #bioconductor, taper edgeR !
+library(edgeR)
 data_cpm = cpm(data)
 
-library(DESeq2) #bioconductor aussi
-
-####Peut-on utiliser une loi de Poisson pour analyser l'expression différentielle ?####
-vectMean = apply(data,1,mean)
-vectVar = apply(data,1,var)
-plot(log(vectMean+1),log(vectVar+1))
-abline(0,1)
+library(DESeq2)
 
 cond = factor(c(rep("per",3),rep("control",3)))
 cnts = data[rowSums(data)>10,]
 
 dds = DESeqDataSetFromMatrix(cnts, DataFrame(cond), ~cond)
-
 dds = DESeq(dds)
-
 res = results(dds)
-#LogFC et p-values
-
-res[order(res$pvalue)[1:100],] #Plus petits p-values
-
-
-table(res$padj<.05) 
-table(res$pvalue<.05/dim(cnts)[1])
-
-par(scipen = 100)
-
-hist(res$pvalue) 
-par(bg = "gray80")
-plotMA(res, ylim = c(-5, 5), colSig = 'red', colNonSig = 'black',xaxt = "n")
-abline(h = pretty(res$log2FoldChange), col = "white", lty = 3)
-
-# puissances de 10 adaptées à tes données
-seq = c(0,2,4,6)
-powers <- 10^seq   # 1, 10, 100, 1000, 10000, 100000, 1e6
-
-abline(v = powers, col = "white", lty = 3)
-
-axis(1, at = powers, labels = parse(text = paste0("10^",seq)))
-
-
-
-
 res = as.data.frame(res)
 
-data$gene = rownames(data)
-res$gene = rownames(res)
+# ------------------------------------------------------------------------------
+# Fusion
+# ------------------------------------------------------------------------------
+data$Name = rownames(data)
+res$Name = rownames(res)
+data_perso = merge(data, res, by = "Name")
 
-data_perso = merge(data, res, by = "gene")
+library(ggplot2)
+library(ggrepel)
+options(bitmapType = "cairo")
 
-data_article = read.delim("~/GSE139659_IPvsctrl.complete.xls.gz")
 
+# Volcano
+res$negLog10P <- -log10(res$pvalue)
+res$significant <- "Non significatif"
+res$significant[!is.na(res$pvalue) & res$pvalue < 0.05 & abs(res$log2FoldChange) >= 1] <- "Significatif"
+
+volcano <- ggplot(res, aes(x = log2FoldChange, y = negLog10P, color = significant)) +
+  geom_point(alpha = 0.6, size = 2) +
+  theme_minimal()
+
+# PCA
+vsd <- vst(dds, blind = FALSE)
+pcaData <- plotPCA(vsd, intgroup = "cond", returnData = TRUE)
+percentVar <- round(100 * attr(pcaData, "percentVar"))
+
+pca_plot <- ggplot(pcaData, aes(PC1, PC2, color = cond)) +
+  geom_point(size=4) +
+  theme_minimal()
+
+# ------------------------------------------------------------------------------
+# Sauvegardes
+# ------------------------------------------------------------------------------
+ggsave(file.path(output_dir, "volcano_plot.png"), volcano, width = 8, height = 6)
+ggsave(file.path(output_dir, "PCA_plot.png"), pca_plot, width = 8, height = 6)
+write.csv(data_perso, file.path(output_dir, "data_perso.csv"))
+
+# Comparaison
+comp = merge(data_perso,data_article, by="Name")
+write.csv(comp, file.path(output_dir, "comparison_means.csv"))
